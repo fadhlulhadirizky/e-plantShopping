25d65857a2a345a24004ca44b9e3f307e00d0e48
Fadhlul Hadi Rizky <fadhlulhadirizky@gmail.com>
2025-10-03 21:25:37 -0400

    initial commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,17 +3,21 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
     addItem: (state, action) => {
-    
+      state.items.push(action.payload);
     },
     removeItem: (state, action) => {
+      state.items = state.items.filter(item => item.name !== action.payload.name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const { name, quantity } = action.payload;
+      const item = state.items.find(item => item.name === name);
+      if (item) {
+        item.quantity = quantity;
+      }
     },
   },
 });
